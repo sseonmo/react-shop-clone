2e9c621bb75b6a9381faacc01cca48b5da7bd3da
sseonmo <mo3509@gmail.com>
2020-06-05 00:30:40 +0900

    [20.06.04][shopping mall site clone] 랜딩페이지 만들기 - 라디오 박스 필터 만들기2

--- client/src/components/views/LandingPage/Sections/Datas.js
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -8,6 +8,17 @@ const continents = [
 	{"_id": 7, name: "Antarctica"}
 ];
 
-export {
-	continents
-}
+const price = [
+	{"_id": 1, name: "Any", "array": []},
+	{"_id": 2, name: "$0 to $199", "array": [0, 199]},
+	{"_id": 3, name: "$200 to $249", "array": [200, 249]},
+	{"_id": 4, name: "$250 to $299", "array": [250, 399]},
+	{"_id": 5, name: "$300 to $349", "array": [300, 349]},
+	{"_id": 6, name: "$350 to $300", "array": [350, 399]},
+	{"_id": 7, name: "$400 to $449", "array": [400, 449]},
+	{"_id": 8, name: "$450 to $499", "array": [450, 499]},
+	{"_id": 9, name: "$500 to $549", "array": [500, 549]},
+	{"_id": 10, name: "$550 to $600", "array": [550, 600]},
+];
+
+export {continents, price}
---
+++ client/src/components/views/LandingPage/Sections/RadioBox.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {Radio, Collapse} from "antd";
+
+const {Panel} = Collapse;
+
+function RadioBox({list, handlerFilters}) {
+
+	const [Value, setValue] = useState(0);
+
+	const renderRadiobox = () => list && list.map((value, index) => (
+		<Radio key={index} value={value._id}>
+			{ value.name}
+		</Radio>
+	));
+
+	const handlerChange = (e) => {
+		setValue(e.target.value);
+		handlerFilters(e.target.value);
+	};
+
+	return (
+		<div>
+			<Collapse defaultActiveKey={['1']}>
+				<Panel header="Price" key="1">
+					<Radio.Group onChange={handlerChange} >
+						{renderRadiobox()}
+					</Radio.Group>
+				</Panel>
+			</Collapse>
+		</div>
+	);
+}
+
+export default RadioBox;
